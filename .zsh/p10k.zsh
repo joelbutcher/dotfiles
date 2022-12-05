e02b11dddc6f3b081f59ff013a3e23128783daf0
Joel Butcher <joel@joelbutcher.co.uk>
2022-12-05 11:41:43 +0000

    update p10k zsh config

--- .zsh/p10k.zsh
+++ .zsh/p10k.zsh
@@ -2,8 +2,5 @@ if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-
-# To customize prompt, run `p10k configure` or edit ~/Dropbox/Mackup/.p10k.zsh.
-[[ ! -f ~/Dropbox/Mackup/.p10k.zsh ]] || source ~/Dropbox/Mackup/.p10k.zsh
+# To customize prompt, run `p10k configure` ork edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k/config.zsh ]] || source ~/.p10k/config.zsh

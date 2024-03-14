f7aed549f04c3f11e9452e267feab0ab6e468468
Joel Butcher <joelbutcher@users.noreply.github.com>
2024-03-14 10:34:29 +0000

    things

--- shell/.zsh/brew.zsh
+++ shell/.zsh/brew.zsh
@@ -1,4 +1,6 @@
 # Python:x
+export PATH="/opt/homebrew/opt/python@3.8/bin:$PATH"
+export PATH="/opt/homebrew/opt/python@3.8/bin:$PATH"
 
-export PATH="/opt/homebrew/opt/python@3.8/bin:$PATH"
-export PATH="/opt/homebrew/opt/python@3.8/bin:$PATH"
+# LibPQ
+export PATH="/usr/local/opt/libpq/bin:$PATH"
